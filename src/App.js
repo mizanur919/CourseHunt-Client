0969e2c9c7b40f64e5b2c01284d7d2e47e5e1726
alamgirjuab <alamgirjuab@gmail.com>
2022-04-24 00:51:21 +0600

    branch alamgir created

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h2>Course Hunt</h2>
         </a>
       </header>
     </div>
