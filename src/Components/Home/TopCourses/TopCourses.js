a9a94f2eca9e987fb7b1b12129f8d223bb7c3c93
mizanur919 <janctgbd@gmail.com>
2022-04-24 23:26:03 +0600

    top courses added

---
+++ src/Components/Home/TopCourses/TopCourses.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TopCourses.css";
+
+const TopCourses = () => {
+  return (
+    <div>
+      <h1>Top Courses</h1>
+    </div>
+  );
+};
+
+export default TopCourses;
