5c10d0faab86a86ce5f574d27d756718dad3984c
mizanur919 <janctgbd@gmail.com>
2022-04-26 00:17:19 +0600

    home page components

---
+++ src/Components/Home/Subscription/Subscription.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Subscription.css";
+
+const Subscription = () => {
+  return (
+    <div>
+      <h1>Subscription</h1>
+    </div>
+  );
+};
+
+export default Subscription;
